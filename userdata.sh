41eb96d992e463db2affb256821968fd9c1462ef
s poorna chandra <poornanaidu1@gmail.com>
2023-12-22 17:11:28 +0530

    terraform

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-yum install ansible -y &>>/opt/userdata.log
+yum install ansible python3.11.-pip.norch -y &>>/opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/SPOORNACHANDRA/ansible-roboshop-v1.git main.yml -e component=rabbitmq &>>/opt/userdata.log
